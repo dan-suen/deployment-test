a63fb07dda05cd5b543f4c795ef6e7d096d56923
Daniel Suen <danielsuen2007@yahoo.ca>
2023-01-19 17:33:58 -0500

    changed sequelize config for elephant sql

--- db/connection.js
+++ db/connection.js
@@ -3,20 +3,20 @@ const { Pool } = require('pg');
 //require('dotenv').config({ path: require('find-config')('.env') })
 
 const dbParams = {
-  connectionString : 'postgres://vyybmdqcukzutt:8ec8b94667c4f88c0a452e628fa7c846f1cf52e6de8997d2e2b91cbe99202ec6@ec2-54-157-79-121.compute-1.amazonaws.com:5432/d4mcojm2fndubn',
+  connectionString : process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
 },
 };
 
 const proParams = {
-  connectionString : 'postgres://vyybmdqcukzutt:8ec8b94667c4f88c0a452e628fa7c846f1cf52e6de8997d2e2b91cbe99202ec6@ec2-54-157-79-121.compute-1.amazonaws.com:5432/d4mcojm2fndubn',
+  connectionString : process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
 },
 };
 
-const db = new Pool(process.env.NODE_ENV === "production" ? proParams : dbParams);
+const db = new Pool(process.env.NODE_ENV);
 
 db.connect();
 console.log(process.env.DB_HOST)
